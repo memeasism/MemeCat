a8ae20e32f72ba0d6539c11a125ae6673036e38b
intLostDev <mavery735@gmail.com>
2022-09-18 18:36:36 -0400

    added install deps scripts

---
+++ installdeps.sh
@@ -0,0 +1,3 @@
+./lit install SinisterRectus/discordia
+git clone https://github.com/Bilal2453/discordia-interactions.git ./deps/discordia-interactions
+git clone https://github.com/GitSparTV/discordia-slash.git ./deps/discordia-slash
